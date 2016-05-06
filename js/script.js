58b98baa3c793565451e1ef7c55e365ffa6c5360
Michael Churchill <michaelchurchill@Michael.local>
2016-05-05 19:22:48 -0500

    cleaned styling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-console.log(Backbone) 
+console.log(Backbone)
 
 //----------------Model------------------//
 
 var HomeModel = Backbone.Model.extend ({
 
-	url: `https://openapi.etsy.com/v2/listings/active.js?includes=Images&api_key=0jiwg9od2dm6mhcggncv05zs&callback=?`
-
+    url: 'https://openapi.etsy.com/v2/listings/active.js?includes=Images,Shop&api_key=aavnvygu0h5r52qes74x9zvo&callback=?'
 })
 
-var DetailModel = Backbone.Model.extend ({ 
+var DetailModel = Backbone.Model.extend ({
 	_makeUrl: function(id) {
-		this.url = `https://openapi.etsy.com/v2/listings/${id}.js?includes=Images&api_key=0jiwg9od2dm6mhcggncv05zs&callback=?`
-	}
+		this.url = 'https://openapi.etsy.com/v2/listings/'+ id +'.js?includes=Images,Shop&api_key=aavnvygu0h5r52qes74x9zvo&callback=?'
+    }
 })
 
 // ---------------Views-------------------//
@@ -26,13 +25,14 @@ var HomeView = Backbone.View.extend({
 		this.model.on("sync", newFunc)
 	},
 
-	events: { 
+	events: {
 		"click img": "_triggerDetailView",
 		'keydown input':'_searchByKeyword'
 	},
 
 	_searchByKeyword: function (keyEvent){
 		var searchTerm = keyEvent.target.value
+        console.log(searchTerm)
 		if(keyEvent.keyCode === 13){
 			location.hash = 'search/' + searchTerm
 		}
@@ -46,33 +46,74 @@ var HomeView = Backbone.View.extend({
 
 	_render: function(input){
 		var dataArray = input.get("results")
-		var gifUrlString = `<div id="head-holder"><div id="header"><a href=""id="logo" placeholder="Search for items or shops">Etsy</a><div id="search-box"><div id="search"><input type="text"id="search"></input></div><div id="go"><button>Press Enter...</button></div></div></div></div><div id="container">`
+		var gifUrlString = `<div id="header">
+			<a href="" id="logo" placeholder="Search for items or shops">Etsy</a>
+			<div id="search-box">
+				<div id="search">
+					<input type="text"id="search">
+					</input>
+				</div>
+				<div id="go">
+					<button>Press Enter...</button>
+				</div>
+			</div>
+		</div>
+        <div id="category-bar">
+          <ul>
+            <a class="categories" href="#search/Clothing & Accessories">
+              <li>Clothing & Accessories</li>
+           </a>
+           <a class="categories" href="#search/Jewelry">
+              <li>Jewelry</li>
+           </a>
+           <a class="categories" href="#search/Craft Supplies & Tools">
+              <li>Craft Supplies & Tools</li>
+           </a>
+           <a class="categories" href="#search/Weddings">
+              <li>Weddings</li>
+           </a>
+           <a class="categories" href="#search/Entertainment">
+              <li>Entertainment</li>
+           </a>
+           <a class="categories" href="#search/Home & Living">
+              <li>Home & Living</li>
+           </a>
+           <a class="categories" href="#search/Kids & Baby">
+              <li>Kids & Baby</li>
+           </a>
+           <a class="categories" href="#search/Vintage">
+              <li>Vintage</li>
+           </a>
+        </ul>
+        </div>
+    </div><div class="item-view">`
 		for (var i = 0; i < dataArray.length - 1; i++) {
 			var img = dataArray[i].Images[0].url_fullxfull
 			console.log(img)
 			console.log(dataArray)
-			gifUrlString += `<div id="itemBlock"><img src="${img}" value="${dataArray[i].listing_id}">\
-							<div id="scrollDescription">${dataArray[i].description.substr(0, 30)}...</div>
-							 <h3 id="cost">${dataArray[i].price}${dataArray[i].currency_code}</h3>
-							<h3 id="quantity" > Quantity:${dataArray[i].quantity}</h3></div>`
+			gifUrlString += `<div class="itemBlock"><a><img class="list-img" src="${img}" value="${dataArray[i].listing_id}"></a>
+                            <div class="info-holds">
+							<div class="scrollDescription ellipsis">${dataArray[i].description.substr(0, 30)}...</div>
+							<span class="madeBy" >${dataArray[i].Shop.shop_name}</span>
+                            <span id="cost">${dataArray[i].price}$</span></div></div>`
 		}
 		console.log(dataArray)
 		this.el.innerHTML = gifUrlString + "</div>"
-		
+
 	}
-	 
+
 })
 
 var DetailView = Backbone.View.extend({
 	el: "#front",
 
-	initialize: function(someModel) { 
+	initialize: function(someModel) {
 		this.model = someModel
 		var boundRender = this._render.bind(this)
 		this.model.on("sync",boundRender)
 	},
 
-	events: { 
+	events: {
 		"click img": "_triggerScrollView",
 		"click button": "_triggerScrollView"
 	},
@@ -85,22 +126,22 @@ var DetailView = Backbone.View.extend({
 	_render: function (){
 		console.log(this.model)
 		var imgObj = this.model.get('results')[0]
-		var htmlString = `<button>Go Back</button>`
+		var htmlString = ``
 		var img = imgObj.Images[0].url_fullxfull
 		console.log(imgObj)
-				 htmlString += `<div id="detailContainer"><div id="imgHolder"><img src="${img}" id="largeImage"></div>\
-							    <div id="description"><h1 id="title">${imgObj.title}</h1>\
+				 htmlString += `<div id="detailContainer"><button class="backb">Go Back</button><div id="imgHolder"><img src="${img}" id="largeImage"></div>\
+							    <div id="description-contains"><h3 id="title">${imgObj.title}</h3>\
 							   <h1 id="shop"></h1><p id="description">${imgObj.description}</p>\
 							   <h3 id="cost">${imgObj.price} ${imgObj.currency_code}</h3>\
-							   <h3 id="quantity" > Quantity: ${imgObj.quantity}</h3></div></div>`
-			
+							   <h4 id="quantity">Quantity: ${imgObj.quantity}</h4></div></div>`
+
 			this.el.innerHTML = htmlString
 	 }
 })
 
 //----------------Router-------------------//
 
-var IphyRouter = Backbone.Router.extend ({
+var Router = Backbone.Router.extend ({
 
 	routes: {
 		'home': 'handleHomeView',
@@ -136,4 +177,4 @@ var IphyRouter = Backbone.Router.extend ({
 
 })
 
-var rtr = new IphyRouter()
+var rtr = new Router()
