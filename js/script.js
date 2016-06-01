395a35cb9e1419bb928264dab5de1d1f2c8af5ba
Michael Churchill <mwinstonch@gmail.com>
2016-05-31 19:56:14 -0500

    no message

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ var HomeView = Backbone.View.extend({
         </div>
         <img class="backgroundPic" src="./Etspic.jpg" />
         <h1 class="sloganTop">Whoever you are, find</h1>
-        <h1 class="sloganBottom">whatever you're into.</h1>
+        <h1 class="sloganBottom">whatever you're into</h1>
         </div><div class="item-view">`
 		for (var i = 0; i < dataArray.length - 1; i++) {
 			var img = dataArray[i].Images[0].url_fullxfull
